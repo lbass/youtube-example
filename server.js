71457aaf8d7299eda24d55a1345161e498741373
Lbass <ehdnjsdl0929@gmail.com>
2019-02-16 05:43:01 +0900

    parameter로 key를 전송할 경우에만 정상 작동

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const server = require('node-http-server');
-
+server.beforeServe=beforeServe;
 server.deploy(
     {
         port:25000,
@@ -7,6 +7,15 @@ server.deploy(
     },
     serverReady
 );
+
+function beforeServe(request, response, body, encoding) {
+    const sKey = request.uri.query.skey;
+    if(!sKey || sKey !== 'xbwmqjrj') {
+        console.error('key missmatch');
+        body.value = 'key missmatch'
+        return;
+    }
+}
 
 function serverReady(server) {
    console.log( `Server on port ${server.config.port} is now up`);
